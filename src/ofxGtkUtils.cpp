3e84c25266489dff955145668988ab69d0665b4e
Philip Whitfield <philip@undef.ch>
2014-09-09 01:30:34 +0200

    color conversion utils

--- src/ofxGtkUtils.cpp
+++ src/ofxGtkUtils.cpp
@@ -1,11 +1,11 @@
 #include "ofxGtkUtils.h"
 
 ofColor toOf(Gdk::Color color){
-	return ofColor(color.get_red(), color.get_green(), color.get_blue());
+	return ofShortColor(color.get_red(), color.get_green(), color.get_blue());
 }
 
 ofColor toOf(Gdk::Color& color){
-	return ofColor(color.get_red(), color.get_green(), color.get_blue());
+	return ofShortColor(color.get_red(), color.get_green(), color.get_blue());
 }
 
 ofFloatColor toOf(Gdk::RGBA color){
@@ -16,3 +16,10 @@ ofFloatColor toOf(Gdk::RGBA& color){
 	return ofFloatColor(color.get_red(), color.get_green(), color.get_blue());
 }
 
+///
+Gdk::Color toGdk(ofColor color){
+	ofShortColor colorShort(color);
+	Gdk::Color ret;
+	ret.set_rgb(colorShort.r, colorShort.g, colorShort.b);
+	return ret;
+}
--- src/ofxGtkUtils.h
+++ src/ofxGtkUtils.h
@@ -2,11 +2,14 @@
 #define OFXGTKUTILS_H
 
 #include <gtkmm.h>
+#include <gdkmm.h>
 #include "ofMain.h"
 
 ofColor toOf(Gdk::Color color);
 ofColor toOf(Gdk::Color& color);
 ofFloatColor toOf(Gdk::RGBA color);
 ofFloatColor toOf(Gdk::RGBA& color);
+
+Gdk::Color toGdk(ofColor color);
 
 #endif // OFXGTKUTILS_H
